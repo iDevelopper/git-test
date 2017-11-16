6d8248a3d979f6c8cb4e83fcb1f5aaaed53815be
Julien Bodet <julien.bodet92@gmail.com>
2017-11-16 07:55:22 -0500

    Add Xcode project

---
+++ git-test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  git-test
+//
+//  Created by Julien BODET on 2017-11-16.
+//  Copyright © 2017 Julien BODET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet private weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.label.text = "Hello, World!"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
