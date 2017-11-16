c2461f63cfdad64b37a0fb16427bdc9ae4a54178
iDevelopper <patrick.bodet4@wanadoo.fr>
2017-11-16 15:26:04 +0100

    Change view background color to blue

--- git-test/ViewController.swift
+++ git-test/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = UIColor.blue
+        
         self.label.text = "Hello, World!"
     }
 
